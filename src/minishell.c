53bceb5817efcaa31bf2aba7928fc5dc7348a282
Vahdet Parlak <vparlak@k2m01s05.42kocaeli.com.tr>
2023-11-05 18:12:32 +0300

    read_vahdet

--- src/minishell.c
+++ src/minishell.c
@@ -1,31 +1,47 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
-int main() {
-    char buffer[2000];
-    char *argv[] =  {"ls", NULL};
-    int fd[2];
-    pipe(fd);
-    int pid = fork();
-    if (pid == 0)
-    {
-        close(3);
-        dup2(4, STDOUT_FILENO);
-        execve("/bin/ls", argv, NULL);
-    }
-    else
-    {
-        close(4);
-        read(3, buffer, 300);
-        int fd2[2];
-        pipe(fd2);
-        pid = fork();
-        if (pid == 0)
-        {
-            close(5);
-            char *argvx[] = {"echo", buffer, NULL};
-            execve("/bin/echo", argvx , NULL);
-        }
-    }
+int	ft_create_file(char *file)
+{
+	int	fd;
+
+	fd = open(file, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	return (fd);
 }
 
+int main()
+{
+	int fd[3];
+
+	char *command[] = {"ls", ">", "vahdet", ">", "burak", "", "cemal", NULL};
+	int i = 0;
+	int j = 0;
+	while (command[i])
+	{
+		if (strcmp(command[i], ">") == 0)
+		{
+			fd[j] = ft_create_file(command[i + 1]);
+			printf("fd = %d\n", fd[j]);
+			j++;
+		}
+		i++;
+	}
+	char str[7];
+	write(fd[0], "bahdet", 6);
+	read(fd[0], str, 6);
+	write(1, str, 6);
+
+
+/* 	char *arg[] = {"ls", NULL};
+	int pid = fork();
+	if (pid > 0)
+		wait(&pid);
+	else
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		execve("/bin/ls", arg, NULL);
+	} */
+}
