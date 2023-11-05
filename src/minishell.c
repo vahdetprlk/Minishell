c2c16c1825d9a68890cab3d7598368a21ca6d8d0
Burak YILMAZ <100238698+Ropnez@users.noreply.github.com>
2023-11-05 19:00:03 +0300

    up

--- src/minishell.c
+++ src/minishell.c
@@ -1,47 +1,41 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 #include <fcntl.h>
-#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
-int	ft_create_file(char *file)
-{
-	int	fd;
+int ft_create_file(char *file) {
+    int fd;
 
-	fd = open(file, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	return (fd);
+    fd = open(file, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    return fd;
 }
 
-int main()
-{
-	int fd[3];
+int main() {
+    int fd[3];
 
-	char *command[] = {"ls", ">", "vahdet", ">", "burak", "", "cemal", NULL};
-	int i = 0;
-	int j = 0;
-	while (command[i])
-	{
-		if (strcmp(command[i], ">") == 0)
-		{
-			fd[j] = ft_create_file(command[i + 1]);
-			printf("fd = %d\n", fd[j]);
-			j++;
-		}
-		i++;
-	}
-	char str[7];
-	write(fd[0], "bahdet", 6);
-	read(fd[0], str, 6);
-	write(1, str, 6);
+    char *command[] = {"ls", ">", "vahdet", ">", "burak", "", "cemal", NULL};
+    int i = 0;
+    int j = 0;
 
+    while (command[i]) {
+        if (strcmp(command[i], ">") == 0) {
+            fd[j] = ft_create_file(command[i + 1]);
+            printf("fd = %d\n", fd[j]);
+            j++;
+        }
+        i++;
+    }
 
-/* 	char *arg[] = {"ls", NULL};
-	int pid = fork();
-	if (pid > 0)
-		wait(&pid);
-	else
-	{
-		dup2(fd[1], STDOUT_FILENO);
-		execve("/bin/ls", arg, NULL);
-	} */
+    char str[10000];
+
+    write(fd[1], "burak", 5);
+    close(fd[1]);
+
+    fd[1] = open("burak", O_RDONLY);
+    read(fd[1], str, 5);
+    
+    write(fd[0], str, 5);
+    close(fd[0]);
+
+    return 0;
 }
