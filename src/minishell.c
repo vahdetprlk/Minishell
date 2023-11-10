9b1f51abfd7e1f0d73a756d38cd13790916952fe
Burak Yilmaz <buryilma@k2m15s02.42kocaeli.com.tr>
2023-11-10 17:43:42 +0300

    promt_check_burak

--- src/minishell.c
+++ src/minishell.c
@@ -2,25 +2,94 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-
-void	ft_prompt_check(char *prompt)
+int ft_word_counter(char* prompt)
 {
-	char **splitted_prompt;
-
-	splitted_prompt = ft_split(prompt, ' ');
 	int i = 0;
-	while (splitted_prompt[i])
-	{
-		printf("%s\n", splitted_prompt[i]);
+	int count = 0;
+	int length = strlen(prompt);
+	int in_quotes = 0;
+
+	while(i < length) {
+		if(prompt[i] == '\"' || prompt[i] == '\'') {
+			in_quotes = !in_quotes;
+		}
+		if((prompt[i] == ' ' && !in_quotes && prompt[i+1] != ' ' && prompt[i+1] != '\0') || 
+		   (prompt[i] == '\"' || prompt[i] == '\'')) {
+			count++;
+		}
+		if((prompt[i] == '|' || prompt[i] == '<' || prompt[i] == '>') && !in_quotes) {
+			count++;
+		}
 		i++;
 	}
 
+	if(prompt[length - 1] != ' ' && !(prompt[length - 1] == '|' && (prompt[length - 2] == ' ' || length == 1))) {
+		count++;
+	}
+
+	return count;
+}
+
+char** split_prompt(char* prompt) {
+    int length = strlen(prompt);
+    char** words = malloc((length + 1) * sizeof(char*));
+    for(int i = 0; i <= length; i++) {
+        words[i] = malloc((length + 1) * sizeof(char));
+    }
+
+    int i = 0, j = 0, k = 0;
+    int in_quotes = 0;
+    while(prompt[i]) {
+        if(prompt[i] == '\"' || prompt[i] == '\'') {
+            words[j][k] = prompt[i];
+            k++;
+            if(in_quotes && k != 0) {
+                words[j][k] = '\0';
+                j++;
+                k = 0;
+            }
+            in_quotes = !in_quotes;
+        } else if((prompt[i] == '|' || prompt[i] == '<' || prompt[i] == '>') && !in_quotes) {
+            if(k != 0) {
+                words[j][k] = '\0';
+                j++;
+                k = 0;
+            }
+            words[j][0] = prompt[i];
+            words[j][1] = '\0';
+            j++;
+        } else if((prompt[i] != ' ' || in_quotes) && prompt[i] != '\0') {
+            words[j][k] = prompt[i];
+            k++;
+        } else if(prompt[i] == ' ' && !in_quotes) {
+            if(k != 0) {
+                words[j][k] = '\0';
+                j++;
+                k = 0;
+            }
+        }
+        i++;
+    }
+    if(k != 0) {
+        words[j][k] = '\0';
+        j++;
+    }
+    words[j] = NULL;
+
+    return words;
 }
 
 void	ft_prompt_hook(char *prompt)
 {
-	ft_prompt_check(prompt);
+	ft_word_counter(prompt);
+	printf("word count: %d\n", ft_word_counter(prompt));
+	char** words = split_prompt(prompt);
+	for(int i = 0; words[i]; i++) {
+		printf("cmd[%d] %s\n",i, words[i]);
+	}
 	free(prompt);
 }
 
